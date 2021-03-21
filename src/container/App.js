b877f6b5dc2afd40949db2d1b599dfd318f37f1e
akramansari1433 <70878208+akramansari1433@users.noreply.github.com>
2021-03-21 22:36:40 +0530

    react hooks

--- src/container/App.js
+++ src/container/App.js
@@ -1,47 +1,50 @@
-import React from 'react'
-import CardList from '../components/CardList'
-import SearchBox from '../components/SearchBox'
-import Scroll from '../components/Scroll'
-import './App.css'
+import React,{useEffect, useState } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
 
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            robots:[],
-            searchfield:''
-        }
+
+function App(){
+    // constructor(){
+    //     super()
+    //     this.state={
+    //         robots:[],
+    //         searchfield:''
+    //     }
+    // }
+    const [robots,setRobots]=useState([]);
+    const [searchfield,setSearchfiel]=useState('');
+    const onSearchChange=(event)=>{
+        setSearchfiel(event.target.value)
     }
-    onSearchChange=(event)=>{
-        this.setState({searchfield:event.target.value})
-    }
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //       .then(response=> response.json())
+    //       .then(users => {this.setState({ robots: users})});
+    // }
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
-          .then(response=> response.json())
-          .then(users => {this.setState({ robots: users})});
-      }
-    
-
-    render(){
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot =>{
+        .then(response=> response.json())
+        .then(users => {setRobots(users)});
+    },[])
+    const filteredRobots = robots.filter(robot =>{
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        if(robots.length===0)
-        {
-            return <h1 className='tc'>Loading...</h1>
-        }
-        else{
-            return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    < CardList robots={filteredRobots} />
-                </Scroll>
-            </div>
-            );
-        }
+    })
+    if(robots.length===0)
+    {
+        return <h1 className='tc'>Loading...</h1>
+    }
+    else{
+        return (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                < CardList robots={filteredRobots} />
+            </Scroll>
+        </div>
+        );
     }
     
 }
